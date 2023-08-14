04913226e682daa8dff427c970fa33dd1a1a71cc
Pranshu Malhotra <pranshudee8@gmail.com>
2023-08-14 18:01:53 +0530

    implement Identity request serializer

---
+++ contact/serializers.py
@@ -0,0 +1,17 @@
+from rest_framework import serializers
+from django.core.validators import RegexValidator
+
+
+class IdentityRequestSerializer(serializers.Serializer):
+    phoneNumber = serializers.CharField(required=False, default='', allow_blank=True, max_length=15, validators=[RegexValidator(
+        regex=r'^\+?1?\d{9,15}$',
+        message="Phone number must be entered in the format '+123456789'. Up to 15 digits allowed."
+    ),])
+    email = serializers.EmailField(required=False, default='', allow_blank=True)
+
+    class Meta:
+        fields = ('phone_number', 'email')
+        extra_kwargs = {
+            'phone_number': {'required': False},
+            'email': {'required': False}
+        }
