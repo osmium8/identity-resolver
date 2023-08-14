2330469fda5b96e44c4e55134cfffca43433b17d
Pranshu Malhotra <pranshudee8@gmail.com>
2023-08-14 20:45:01 +0530

    set url path for app

--- project/urls.py
+++ project/urls.py
@@ -4,6 +4,7 @@ from drf_spectacular.views import SpectacularAPIView, SpectacularSwaggerView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('api/contact', include("contact.urls")),
     path("api/schema/", SpectacularAPIView.as_view(), name="schema"),
     path("api/schema/docs/", SpectacularSwaggerView.as_view(url_name="schema")),
 ]
