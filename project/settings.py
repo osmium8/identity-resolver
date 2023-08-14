95550d0b3f9cfc04b9c70aaaa715385ab46ae79e
Pranshu Malhotra <pranshudee8@gmail.com>
2023-08-14 17:04:29 +0530

    init app

--- project/settings.py
+++ project/settings.py
@@ -37,6 +37,12 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    # External Packages
+    'rest_framework',
+
+    # Project applications
+    'contact'
 ]
 
 MIDDLEWARE = [
